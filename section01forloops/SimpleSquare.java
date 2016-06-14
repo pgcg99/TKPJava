c49a839623671c1e1c77aab5fd93c2fafc84cc33
pgcg99 <pgcg99@yahoo.com>
2016-06-14 20:17:50 +0000

    Add files via upload

---
+++ section01forloops/SimpleSquare.java
@@ -0,0 +1,21 @@
+package org.teachingkidsprogramming.section01forloops;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class SimpleSquare
+{
+  public static void main(String[] args) throws Exception
+  {
+    //  Show the tortoise.
+    //  Make the tortoise move as fast as possible --#6
+    //  Do the following 4 times --#5.1
+    //      Change the pen color of the line the tortoise draws to blue --#4
+    //      Move the tortoise 50 pixels --#2
+    Tortoise.move(50);
+    //      Turn the tortoise to the right (90 degrees) --#3
+    //  Repeat --#5.2
+    //
+    //  (Optional): Sign your work using the Virtual Proctor 
+    //  See your work at http://virtualproctor.tkpjava.org
+  }
+}
